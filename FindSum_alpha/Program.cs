06c6a7d273bb2b1c825bcad0710c777490a02d76
Chris Niehaus <cln2482@outlook.com>
2017-06-16 11:46:55 -0500

    Init push

---
+++ FindSum_alpha/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSum_alpha
+{
+
+    class Program
+    {
+        string space = " ";
+
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            app.matchSum(8, 4, -1, 4, 4, 9);
+            Console.ReadLine();
+        }
+
+        private void matchSum(int sumReq, int size, int b, int c, int d, int f)
+        {
+            int[] numbers = new int[size];
+
+            numbers[0] = b;
+            numbers[1] = c;
+            numbers[2] = d;
+            numbers[3] = f;
+
+            foreach (int testNum in numbers)
+            {
+                int comp = sumReq - testNum;                
+
+                if (numbers.Contains(comp))
+                {
+                    Console.WriteLine("Found:  " + testNum.ToString() + space + "+" + space + comp.ToString() + space + "=" + space + sumReq);
+                }
+
+            }
+
+      
+        }
+    }
+}
